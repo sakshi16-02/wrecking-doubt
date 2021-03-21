6b0d7a8d9a43aaeb8abc469f86f52a843a580918
sakshi mehta <2018pis5341@mnit.ac.in>
2021-03-21 09:17:14 +0530

    inheritance cant be used

--- Ball.js
+++ Ball.js
@@ -1,5 +1,27 @@
-class Ball extends BaseClass{
+// class Ball extends BaseClass{
+//     constructor(x,y){
+//        super(x,y);
+//     }
+// }
+
+class Ball{
     constructor(x,y){
-       super(x,y);
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1
+        }
+        this.body=Bodies.circle(x,y,40,options);
+        World.add(world,this.body);
     }
-}
+    
+    display(){
+        push();
+        translate(this.body.position.x,this.body.position.y)
+        ellipseMode(RADIUS);
+        fill("pink");
+        ellipse(0,0,40,40);
+        pop();
+    }
+    }
